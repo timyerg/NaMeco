2ee2b19ae3b404ad433fb849991fbe5e34cc9e27
timyerg <timyerg@gmail.com>
2024-06-10 15:53:31 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,4 @@ setup(
     author_email='timyerg@gmail.com',
     license='Apache-2.0',
     packages=['nameco'],
-    install_requires=['pandas>=2.2',
-		      'python>=3.10.8',
-		      'chopper>=0.7',
-		      'racon>=1.5',
-		      'medaka>=1.11',
-		      'minimap2>=2.28',
-		      'biopython>=1.83',
-		      'matplotlib>=3.8',
-		      'blast>=2.15',
-		      'spoa>=4.1',
-		      'scikit-learn>=1.5',
-		      'umap-learn>=0.5'],)
+     )
